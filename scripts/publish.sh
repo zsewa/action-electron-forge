b7679433962a5ff404440e9841620714fc9bca32
Zeno Sewald <13609987+zsewa@users.noreply.github.com>
2024-02-20 19:20:18 +0100

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
-#
-# Environment variables:
-#
-# * `NODE_INSTALLER`: Which Node.js package manager to use (`yarn` or `npm`). Defaults to detecting
-#   which package manager was used on the app via `yarn-or-npm` (which is used by Electron Forge).
-#
 
 set -e
 
-args=()
-
-if [[ "$NODE_INSTALLER" = "npm" ]]; then
-  npm run publish
-elif [[ "$NODE_INSTALLER" = "yarn" ]]; then
-  yarn publish
-else
-  "$(npm bin)"/yarn-or-npm run publish"
-fi
+npx electron-forge publish
